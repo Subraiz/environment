16b54aee6655686e40e96b015bddb598a1df9e5c
Subraiz <subraizahmed@gmail.com>
2021-04-26 14:30:10 -0400

    publiish

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import { Header } from "../../components";
 import Store from "../../store.json";
 
 const Home = () => {
-  console.log(Store);
+  const renderPosts = () => {
+    return Store.posts.map((post, i) => {
+      const { photo, desc, shortContent, userId, id } = post;
+      return (
+        <Link key={i} to={`/post/${id}`} style={{ color: "black" }}>
+          <div className={styles.post}>
+            <img src={photo} alt="desc-tag" />
+            <div className={styles.postInfo}>
+              <h2>{desc}</h2>
+              <p>{shortContent}</p>
+            </div>
+            <p className={styles.userId}>Created by {userId}</p>
+          </div>
+        </Link>
+      );
+    });
+  };
   return (
     <div>
       <Header />
@@ -14,6 +31,7 @@ const Home = () => {
             <p>Start A New Post!</p>
             <button>Post</button>
           </div>
+          <div className={styles.posts}>{renderPosts()}</div>
         </div>
         <div className={styles.booksContainer}></div>
       </div>
